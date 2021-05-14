6367433525193cd4ae63d0c6c76c268e85dc64dd
manibalan174 <45997825+manibalan174@users.noreply.github.com>
2021-05-14 12:38:23 +0530

    Add files via upload
    string declaration and literal  and Acess

---
+++ String1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringPart1
+{
+    class Program
+    {
+            public  string masg2 = String.Empty;
+            public  string masg3 = "";
+            public   string str5 = null;
+        public static void strMenthod(string FirstName, string LastName, string Location)
+        {
+            if(string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName) || string.IsNullOrEmpty(Location))
+            {
+                throw new Exception("Data Are Empty Please Fill information");
+            }
+            string Name = FirstName+" "+LastName;
+            string Role = "It's Worked in \" SoftWare Devloper \" in this company.";
+            string EmpInfo = string.Format("Empolyee Name : {0}\nEmpolyee Location : {1}\nEmployee Role : {2}",Name,Location,Role);
+            Console.WriteLine(EmpInfo);
+            string Product = @" "" TaxBandids "" \ "" Express Tax "" \ "" PayWow ""  ";   
+             Console.WriteLine("Company Product : "+Product);
+            // Console.WriteLine("Enter"+masg2);
+             //Console.WriteLine("Enter"+masg3);
+            // Console.WriteLine("Enter"+str5);
+        }   
+        static void Main(string[] args)
+        {   Console.Write("Enetr Frist Name : ");
+            string n = Console.ReadLine();
+             Console.Write("Enetr Last Name : ");
+            string C = Console.ReadLine();
+            Console.Write("Enetr Location : ");
+            string l = Console.ReadLine();
+             strMenthod(n,C,l);
+         }
+    }
+}
